d5b365df55e555aa17cfb495a39366e8623953bd
Xiaobo Sun <xiaobosun@XiaobodeiMac-Pro.local>
2023-11-04 20:51:10 +0800

    modify mysql

--- Example01/install__mysql.sh
+++ Example01/install__mysql.sh
@@ -31,7 +31,7 @@ get_host_info() {
       ;;
     3)
       clear
-      echo $MEM_toal
+      echo $MEM_Capacity
       ;;
     4)
       clear
@@ -58,12 +58,12 @@ Installation_Methods() {
   case $num in
   1)
     read -p "是否使用源码部署Y/N" n
-    if [ $n == y ]; then
+    if [ $n = y ]; then
       printf "先决条件，检查是否满足安装环境... \n 内存满足4G以上,磁盘空间大于30G以上 \n"
       fdisk_total=$(df -h | grep /$ | awk '{print "磁盘总容量:" $2}' | awk -F: '{print $2}' | grep -oP '\d*\.\d+')
       MEM_total=$(free -h | grep Mem | awk '{print "总内存:" $2}' | awk -F: '{print $2}' | grep -oP '\d*\.\d+')
       if
-        [ $fdisk_total -ge 30 ] &&  [$MEM_total -ge 4 ]
+        [ $fdisk_total 30 ] >= && [ $MEM_total 4 ] >=
       then
         echo "恭喜您满足环境可以执行安装......."
       else
@@ -74,10 +74,91 @@ Installation_Methods() {
     fi
     ;;
   2)
-    echo "使用yum|dnf 安装"
-    ;;
-  *)
-    echo "输入错误"
+    #选择要您所需要安装的版本
+    read -p "选择您所需要的版本版本：例如mysql、mariadb数据库： " string
+    case $string in
+    mysql)
+      echo -e "\033[34m install mysql-server \033[0m"
+      OS_vresion=$(cat /etc/redhat-release | awk -F. '{print $1}' | awk '{print $4}')
+      if [ $OS_vresion == 7 ]; then
+        if [ -f /etc/yum.repos.d/mysql* ]; then
+          echo -e "\033[34m install mysql \033[0m"
+          yum -y install mysql-server mysql
+          echo "已安装完成！请初始化mysql并设置密码"
+        else
+          yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-11.noarch.rpm
+          yum -y install mysql*
+        fi
+      elif [ $OS_vresion == 8 ];then
+        if [ -f /etc/yum.repos.d/mysql* ]; then
+          echo -e "\033[34m install mysql \033[0m"
+          yum -y install mysql-server mysql
+          echo "已安装完成！请初始化mysql并设置密码"
+        else
+          yum -y install https://dev.mysql.com/get/mysql80-community-release-el8-9.noarch.rpm
+          yum -y install mysql*
+          echo "已安装完成！请初始化mysql并设置密码"
+        fi
+      else
+        echo "对不起系统不兼容"
+      fi
+      ;;
+    mariadb)
+      if [ $OS_vresion == 7 ]; then
+        if [ -f /etc/yum.repos.d/mariadb* ]; then
+          echo "正在安装......"
+          yum -y install MariaDB-server MariaDB-client >/dev/null
+          echo “安装完成”
+        fi
+        echo "正在配置源......."
+        cat >/etc/yum.repos.d/mariadb.repo <<EOF
+# MariaDB 11.1 CentOS repository list - created 2023-11-04 10:26 UTC
+# https://mariadb.org/download/
+[mariadb]
+name = MariaDB
+# rpm.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+# baseurl = https://rpm.mariadb.org/11.1/centos/\$releasever/\$basearch
+baseurl = https://mirrors.neusoft.edu.cn/mariadb/yum/11.1/centos/\$releasever/\$basearch
+module_hotfixes = 1
+# gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgkey = https://mirrors.neusoft.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
+gpgcheck = 1
+EOF
+        echo "正在安装......"
+        yum -y install MariaDB-server MariaDB-client >/dev/null
+        echo “安装完成”
+      elif [ $OS_vresion == 8 ]; then
+        if [ -f /etc/yum.repos.d/mariadb* ]; then
+          echo "正在安装......"
+          yum -y install MariaDB-server MariaDB-client >/dev/null
+          echo “安装完成”
+        fi
+        echo "正在配置源......."
+        cat >/etc/yum.repos.d/mariadb.repo <<EOF
+# MariaDB 11.1 CentOS repository list - created 2023-11-04 10:33 UTC
+# https://mariadb.org/download/
+[mariadb]
+name = MariaDB
+# rpm.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+# baseurl = https://rpm.mariadb.org/11.1/centos/\$releasever/\$basearch
+baseurl = https://mirrors.neusoft.edu.cn/mariadb/yum/11.1/centos/\$releasever/\$basearch
+# gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgkey = https://mirrors.neusoft.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
+gpgcheck = 1
+EOF
+        echo "正在安装......"
+        dnf -y install MariaDB-server MariaDB-client >/dev/null
+        echo “安装完成”
+      fi
+      echo "install mariadb-server"
+      ;;
+    *)
+      echo "输入错误"
+      ;;
+    *)
+      echo "输入错误,请重新输入...."
+      ;;
+    esac
     ;;
   esac
 }
@@ -86,9 +167,14 @@ init_Safety() {
   case $num1 in
   1)
     echo "源码安装"
+    echo "抱歉还没上线。。。。"
     ;;
   2)
     echo "二进制安装"
+    mysql_secure_installation
+    if [ $? ！= 0 ]; then
+      printf "初始化错误...未找到mysql.sock文件，请启动mysql服务...\n 你可以使用systemctl  start mysqld 或者systemctl status mysqld"
+    fi
     ;;
   3)
     echo "预编译二进制安装"
@@ -104,12 +190,23 @@ service_Manager() {
   echo "======2、 停止服务======="
   echo "======3、 重启服务======="
   echo "======4、 输入错误======="
-  read -p "请输入您要执行服务:" n
-  case $n in
-  1) start ;;
-  2) stop ;;
-  3) restart ;;
-  *) Error ;;
+  read -p "请输入您要执行服务:" string
+  case $string in
+  start)
+    systmectl start mysqld
+    ;;
+  stop)
+    systmectl stop mysqld
+    ;;
+  restart)
+    systmectl restart mysqld
+    ;;
+  status)
+    systmectl status mysqld
+    ;;
+  *)
+    echo -e "\033[31m Error输入错误；请重新输入.. start|stop| restart| status \033[0m"
+    ;;
   esac
 }
 Exit_Program() {
@@ -135,6 +232,6 @@ case $num in
   Exit_Program
   ;;
 *)
-  echo "输入错误；请重新输入"
+  echo "输入错误；请重新输入...."
   ;;
 esac
